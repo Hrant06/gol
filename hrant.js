51ca8968aeea7e884cceafcd14d69cef5a9e305b
lilitbs <lilit.b.s@tumo.org>
2021-07-03 16:56:37 +0400

    comment

---
+++ hrant.js
@@ -0,0 +1,147 @@
+
+// var side = 30;
+// var matrix = []
+
+// for(var i = 0; i < 30; i++){
+//     matrix[i] = []
+//     for(var j = 0; j < 50; j++){
+//         matrix[i][j] = Math.floor(Math.random()*4)
+//     }
+// }
+
+function generator(matLen, gr, grEat,pre,d,q) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }for (let i = 0; i < pre; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < d; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }for (let i = 0; i < q; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    
+    return matrix;
+}
+
+let side = 20;
+
+let matrix = generator(30, 250, 80,35,50,40);
+
+var grassArr = []
+var grassEaterArr = []
+var predatorArr = []
+var didArr = []
+var qarArr = []
+
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y);
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                var grEat = new GrassEater(x, y)
+                grassEaterArr.push(grEat)
+            }
+            else if (matrix[y][x] == 3) {
+                var predat = new Predator(x, y)
+                predatorArr.push(predat)
+            }
+            else if (matrix[y][x] == 4) {
+                var did = new Did(x, y)
+                didArr.push(did)
+            }
+            else if (matrix[y][x] == 5) {
+                var qar = new Qar(x, y)
+                qarArr.push(qar)
+            }
+        }
+    } 
+}
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 3) {
+                fill("blue");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("red");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 5) {
+                fill("black");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].mul();
+
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul();
+        grassEaterArr[i].eat();
+    }
+    for (var i in predatorArr) {
+        predatorArr[i].mul();
+        predatorArr[i].eat();
+    }
+    for (var i in didArr) {
+        didArr[i].die()
+    }
+    
+}
+
+
